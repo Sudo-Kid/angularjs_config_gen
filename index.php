1c8da2300a36d9638528dbcaf08f0560cb3455c2
Emett Speer <speer.emett@gmail.com>
2015-11-24 04:23:17 -0800

    Added footer to page

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <span class="author">The-Kid</span>
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Bootstrap 101 Template</title>
 
@@ -69,5 +70,13 @@ My Test!
 Second Test!!
 	</pre>
 </div>
+
+<footer>
+	<div>
+		::before
+		<p>This page is powered by the code on <a href="https://github.com/the-kid89/angularjs_config_gen">GitHub</a></p>
+		::after
+	</div>
+</footer>
 </body>
 </html>
